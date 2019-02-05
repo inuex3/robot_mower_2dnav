4e0a323155112d8c39717ceca329293f86afe43e
Kosuke Inoue <mmts0106@gmail.com>
2019-02-06 01:43:46 +0900

    Added P controll for angular velocity

--- scripts/cmd_to_rc.py
+++ scripts/cmd_to_rc.py
@@ -5,8 +5,6 @@ import rospy
 from geometry_msgs.msg import Twist
 from mavros_msgs.msg import OverrideRCIn
 import tf
-
-import tf
 from nav_msgs.msg import Odometry
 
 class Publishsers():
@@ -15,48 +13,54 @@ class Publishsers():
         self.publisher = rospy.Publisher('/mavros/rc/override', OverrideRCIn, queue_size = 1)
         #self.max_x = rospy.get_param("~coverage_spacing", 0.4)
         #self.min_x = rospy.get_param("~coverage_spacing", 0.4)
-        self.max_vel_theta = 0.4#= rospy.get_param("~coverage_spacing", 0.4)
+        self.max_vel_theta = 0.3#= rospy.get_param("~coverage_spacing", 0.4)
         # messageの型を作成
         self.RC_msg = OverrideRCIn()
         self.cmd_vel = Twist()
-        self.current_time = rospy.Time.now()
-        self.prev_time = rospy.Time.now()
+        self.current_time = rospy.get_time()
+        self.prev_time = rospy.get_time()
         self.prev_odom = Odometry()
         self.P_gain_x = 200
-        self.P_gain_theta = 200
+        self.P_gain_theta = 350
 
     def make_msg(self, cmd_vel, odom):
         # 処理を書く
-        self.current_time = rospy.Time.now()
-        #normalized_vel_x = (odom.pose.position.x - self.prev_odom.pose.position.x)/ (self.current_time - self.prev_time)
-        vel_rot = (odom.pose.orientation - self.prev_odom.pose.orientation)/ (self.current_time - self.prev_time)
-        normalized_vel_theta = tf.transformations.euler_from_quaternion(vel_rot)
-        print(normalized_vel_theta[2])
-        self.RC_msg.channels = [normalized_vel_theta-cmd_vel.angular.z*angular_gain, 1400 - cmd_vel.linear.x*linear_gain, 0, 0, 0, 0, 0, 0] #1:steering,2:thrust
-	print(self.RC_msg.channels)
+        self.current_time = rospy.get_time()
+        if ((self.current_time - self.prev_time) > 0.5):
+            self.RC_msg.channels = [0, 0, 0, 0, 0, 0, 0, 0] #1:steering,2:thrust
+        else: 
+            self.prev_odom.pose.pose.orientation.w = - self.prev_odom.pose.pose.orientation.w
+            orientation_diff = tf.transformations.quaternion_multiply((odom.pose.pose.orientation.x, odom.pose.pose.orientation.y, odom.pose.pose.orientation.z, odom.pose.pose.orientation.w),(self.prev_odom.pose.pose.orientation.x, self.prev_odom.pose.pose.orientation.y, self.prev_odom.pose.pose.orientation.z, self.prev_odom.pose.pose.orientation.w))       
+            normalized_vel_theta = tf.transformations.euler_from_quaternion(orientation_diff)
+            if (normalized_vel_theta[2] > 0):
+                theta = 50
+            elif (normalized_vel_theta[2] < 0):
+                theta = -50
+            elif (normalized_vel_theta[2] == 0):
+                theta = 0
+            self.RC_msg.channels = [1500 - theta - (cmd_vel.angular.z - normalized_vel_theta[2]/((self.current_time - self.prev_time)*self.max_vel_theta))*self.P_gain_theta, 1400 - cmd_vel.linear.x*self.P_gain_theta, 0, 0, 0, 0, 0, 0] #1:steering,2:thrust
+            #print("cmd"+str(cmd_vel.angular.z))
+            #print("vel"+str(normalized_vel_theta[2]/(self.current_time - self.prev_time)))
+            print("diff"+str(cmd_vel.angular.z-normalized_vel_theta[2]/(self.current_time - self.prev_time)))
         self.prev_odom = odom
-
-    def send_msg(self):
-        # messageを送信
+        self.prev_time = self.current_time       
         self.publisher.publish(self.RC_msg)
 
 class Subscribe_publishers():
     def __init__(self, pub):
         # Subscriberを作成
         self.subscriber = rospy.Subscriber('/cmd_vel', Twist, self.callback, queue_size = 1)
-        self.subscriber = rospy.Subscriber('/odometry/filtered', Odometry, self.odom_callback)
+        self.odom_subscriber = rospy.Subscriber('/odom', Odometry, self.odom_callback)
         # messageの型を作成
         # publish
-	self.pub = pub
-	self.odom = Odometry()
+        self.pub = pub
+        self.odom = Odometry()
 
     def callback(self, cmd_vel):
         #callback時の処理
         self.pub.make_msg(cmd_vel, self.odom)
-        self.pub.send_msg()
 
     def odom_callback(self, msg):
-        print(msg)
         #callback時の処理
         self.odom = msg
 
