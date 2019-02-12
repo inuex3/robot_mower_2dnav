76b2b08bdd215aa072a8df3b471b02ab18eb63ea
Kosuke Inoue <mmts0106@gmail.com>
2019-02-12 14:35:02 +0900

    Parameter changed

--- scripts/cmd_to_rc.py
+++ scripts/cmd_to_rc.py
@@ -17,8 +17,8 @@ class Publishsers():
         self.current_time = rospy.get_time()+0.1
         self.prev_time = rospy.get_time()
         self.prev_odom = Odometry()
-        self.P_gain_x = 200
-        self.P_gain_theta = 200
+        self.P_gain_x = 300
+        self.P_gain_theta = 100
 
     def make_msg(self, cmd_vel, odom):
         # 処理を書く
@@ -29,19 +29,19 @@ class Publishsers():
             self.prev_odom.pose.pose.orientation.w = - self.prev_odom.pose.pose.orientation.w
             orientation_diff = tf.transformations.quaternion_multiply((odom.pose.pose.orientation.x, odom.pose.pose.orientation.y, odom.pose.pose.orientation.z, odom.pose.pose.orientation.w),(self.prev_odom.pose.pose.orientation.x, self.prev_odom.pose.pose.orientation.y, self.prev_odom.pose.pose.orientation.z, self.prev_odom.pose.pose.orientation.w))       
             normalized_vel_theta = tf.transformations.euler_from_quaternion(orientation_diff)
-            if (normalized_vel_theta[2] > 0):
+            if (cmd_vel.angular.z > 0):
                 theta = 50
-            elif (normalized_vel_theta[2] < 0):
+            elif (cmd_vel.angular.z < 0):
                 theta = -50
-            elif (normalized_vel_theta[2] == 0):
+            elif (cmd_vel.angular.z == 0):
                 theta = 0
             if (cmd_vel.linear.x > 0):
                 x = 50
             elif (cmd_vel.linear.x < 0):
-                x = -50
+                x = -80
             elif (cmd_vel.linear.x == 0):
                 x = 0                
-            self.RC_msg.channels = [1500 + theta + (cmd_vel.angular.z - normalized_vel_theta[2]/((self.current_time - self.prev_time)*self.max_vel_theta))*self.P_gain_theta, 1500 - x - cmd_vel.linear.x*self.P_gain_theta, 0, 0, 0, 0, 0, 0] #1:steering,2:thrust
+            self.RC_msg.channels = [1500 - theta - (cmd_vel.angular.z - normalized_vel_theta[2])/(self.current_time - self.prev_time)*self.P_gain_theta, 1500 - x - cmd_vel.linear.x*self.P_gain_theta, 0, 0, 0, 0, 0, 0] #1:steering,2:thrust
             #print("cmd"+str(cmd_vel.angular.z))
             #print("vel"+str(normalized_vel_theta[2]/(self.current_time - self.prev_time)))
             #print("diff"+str(cmd_vel.angular.z-normalized_vel_theta[2]/(self.current_time - self.prev_time)))
