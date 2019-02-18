8f2c0a1eada846797af2f76f908b309f8cba1bd2
Kosuke Inoue <mmts0106@gmail.com>
2019-02-19 01:19:14 +0900

    PID and raw md

--- scripts/cmd_to_rc_pid.py
+++ scripts/cmd_to_rc_pid.py
@@ -19,8 +19,14 @@ class Publishsers():
         self.current_time = 0
         self.prev_time = 0
         self.prev_odom = Odometry()
-        self.gain_x = 100
-        self.gain_theta = 100      
+        self.P_gain_x = 5
+        self.P_gain_theta = 15
+        self.I_gain_x = 5
+        self.I_gain_theta = 15
+        self.D_gain_x = 5
+        self.D_gain_theta = 15
+        self.I_error_x = 0
+        self.I_error_theta = 0        
         self.prev_RC_theta = 0
         self.prev_RC_x = 0
         self.prev_error_theta = 0
@@ -28,6 +34,11 @@ class Publishsers():
 
     def make_msg(self, cmd_vel, odom):
         # 処理を書く
+        self.current_time = rospy.get_time()
+        self.prev_odom.pose.pose.orientation.w = - self.prev_odom.pose.pose.orientation.w
+        orientation_diff = tf.transformations.quaternion_multiply((odom.pose.pose.orientation.x, odom.pose.pose.orientation.y, odom.pose.pose.orientation.z, odom.pose.pose.orientation.w),(self.prev_odom.pose.pose.orientation.x, self.prev_odom.pose.pose.orientation.y, self.prev_odom.pose.pose.orientation.z, self.prev_odom.pose.pose.orientation.w))       
+        theta_diff = tf.transformations.euler_from_quaternion(orientation_diff)
+        position_diff = ((odom.pose.pose.position.x - self.prev_odom.pose.pose.position.x)*(odom.pose.pose.position.x - self.prev_odom.pose.pose.position.x) + (odom.pose.pose.position.y - self.prev_odom.pose.pose.position.y) * (odom.pose.pose.position.y - self.prev_odom.pose.pose.position.y)) ** 0.5   
         if (cmd_vel.angular.z > 0):
             theta = 1450
         elif (cmd_vel.angular.z < 0):
@@ -37,15 +48,35 @@ class Publishsers():
         if (cmd_vel.linear.x > 0):
             x = 1400
         elif (cmd_vel.linear.x < 0):
-            x = 1600
+            x = 1650
         elif (cmd_vel.linear.x == 0.0):
             x = 1500 
-        RC_theta = cmd_vel.angular.z * self.gain_theta
-        RC_x = cmd_vel.linear.x * self.gain_x
+        if self.prev_RC_msg.channels[1] > 1500:
+            position_diff = - position_diff
+        error_theta = cmd_vel.angular.z - theta_diff[2]/(self.current_time - self.prev_time)
+        error_x = cmd_vel.linear.x - position_diff / (self.current_time - self.prev_time)
+        D_error_theta = (error_theta - self.prev_error_theta) / (self.current_time - self.prev_time)
+        D_error_x = (error_x - self.prev_error_x) / (self.current_time - self.prev_time)
+        RC_theta = self.prev_RC_theta + error_theta * self.P_gain_theta - D_error_theta * self.D_gain_theta + self.I_error_theta * self.I_gain_theta
+        RC_x = self.prev_RC_x + error_x * self.P_gain_x - D_error_x * self.D_gain_x + self.I_error_x * self.I_gain_x
         self.RC_msg.channels = [theta - RC_theta, x - RC_x, 0, 0, 0, 0, 0, 0] #1:steering,2:thrust
+        self.prev_RC_theta = RC_theta
+        self.prev_RC_x = RC_x
+        self.prev_error_theta = error_theta
+        self.prev_error_x = error_x
+        self.prev_odom = odom
+        self.prev_RC_msg = self.RC_msg
+        self.prev_time = self.current_time
+        self.I_error_theta = self.I_error_theta + error_theta
+        self.I_error_x = self.I_error_x + error_x
+
+    def reset_I_error(self):
+        self.I_error_x = 0
         
     def send_msg(self, time_now, cb_time):
-        if (time_now - cb_time) > 1.5:
+        if (self.RC_msg.channels[0] < 1000 or self.RC_msg.channels[1] < 1000):
+            self.RC_msg.channels = [1500, 1500, 0, 0, 0, 0, 0, 0] #1:steering,2:thrust
+        if (time_now - cb_time) > 2.0:
             self.RC_msg.channels = [0, 0, 0, 0, 0, 0, 0, 0] #1:steering,2:thrust
             self.prev_RC_theta = 0   
             self.prev_RC_x = 0
@@ -65,6 +96,7 @@ class Subscribe_publishers():
     def callback(self, cmd_vel):
         self.cb_time = rospy.get_time() 
         self.cmd_vel = cmd_vel       
+        self.pub.reset_I_error()
     
     def odom_callback(self, odom):
         #callback時の処理
