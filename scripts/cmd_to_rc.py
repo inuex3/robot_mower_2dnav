284d146d95cb285b205f1fe2eae4f03006ea6a5e
Kosuke Inoue <mmts0106@gmail.com>
2019-02-18 19:49:02 +0900

    pid

--- scripts/cmd_to_rc.py
+++ scripts/cmd_to_rc.py
@@ -21,8 +21,15 @@ class Publishsers():
         self.prev_odom = Odometry()
         self.P_gain_x = 5
         self.P_gain_theta = 15
+        self.I_gain_x = 5
+        self.I_gain_theta = 15
+        self.D_gain_x = 5
+        self.D_gain_theta = 15
+        self.I_error_theta = 0
         self.prev_RC_theta = 0
         self.prev_RC_x = 0
+        self.prev_error_theta = 0
+        self.prev_error_x = 0
 
     def make_msg(self, cmd_vel, odom):
         # 処理を書く
@@ -45,16 +52,25 @@ class Publishsers():
             x = 1500 
         if self.prev_RC_msg.channels[1] > 1500:
             position_diff = - position_diff
-        RC_theta = self.prev_RC_theta + (cmd_vel.angular.z - theta_diff[2]/(self.current_time - self.prev_time))*self.P_gain_theta
-        RC_x = self.prev_RC_x + (cmd_vel.linear.x - position_diff / (self.current_time - self.prev_time))*self.P_gain_x
-        #print("x"+str(position_diff / (self.current_time - self.prev_time)))
-        #print("z"+str(cmd_vel.angular.z - theta_diff[2]/(self.current_time - self.prev_time)))
+        error_theta = cmd_vel.angular.z - theta_diff[2]/(self.current_time - self.prev_time)
+        error_x = cmd_vel.linear.x - position_diff / (self.current_time - self.prev_time)
+        D_error_theta = (error_theta - self.prev_error_theta) / (self.current_time - self.prev_time)
+        D_error_x = (error_x - self.prev_error_x) / (self.current_time - self.prev_time)
+        RC_theta = self.prev_RC_theta + error_theta * self.P_gain_theta - D_error_theta * self.D_gain_theta + self.I_error_theta * self.I_gain_theta
+        RC_x = self.prev_RC_x + error_x * self.P_gain_x - D_error_x * self.D_gain_x + self.I_error_x * self.I_gain_x
         self.RC_msg.channels = [theta - RC_theta, x - RC_x, 0, 0, 0, 0, 0, 0] #1:steering,2:thrust
         self.prev_RC_theta = RC_theta
         self.prev_RC_x = RC_x
+        self.prev_error_theta = error_theta
+        self.prev_error_x = error_x
         self.prev_odom = odom
         self.prev_RC_msg = self.RC_msg
         self.prev_time = self.current_time
+        self.I_error_theta = self.I_error_theta + error_theta
+        self.I_error_x = self.I_error_x + error_x
+
+    def reset_I_error(self):
+        self.I_error_x = 0
         
     def send_msg(self, time_now, cb_time):
         if (self.RC_msg.channels[0] < 1000 or self.RC_msg.channels[1] < 1000):
@@ -78,7 +94,8 @@ class Subscribe_publishers():
 
     def callback(self, cmd_vel):
         self.cb_time = rospy.get_time() 
-        self.cmd_vel = cmd_vel        
+        self.cmd_vel = cmd_vel       
+        self.pub.reset_I_error()
     
     def odom_callback(self, odom):
         #callback時の処理
