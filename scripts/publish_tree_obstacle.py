95c9a1c4d922619e5ebf2ca90190ba74b31d94dc
Kosuke Inoue <mmts0106@gmail.com>
2019-02-21 13:19:47 +0900

    save

--- scripts/publish_tree_obstacle.py
+++ scripts/publish_tree_obstacle.py
@@ -54,42 +54,43 @@ class Publishsers():
                     tan_angle_x = camera_param[0][0]*(bbox.xmin+bbox.xmax)/2+camera_param[0][1]*(bbox.ymin+bbox.ymax)/2+camera_param[0][2]*1
                     distance_y = - distance_x * tan_angle_x
                     angle_x = math.atan(tan_angle_x)  
+                    if abs(math.degrees(angle_x)) < 30 and distance_x > 1.0:
                     # Add point obstacle
-                    self.obstacle_msg.obstacles.append(ObstacleMsg())
-                    self.obstacle_msg.obstacles[i].header.stamp, self.obstacle_msg.obstacles[i].header.frame_id = rospy.Time.now(), Image.header.frame_id     
-                    self.obstacle_msg.obstacles[i].id = i
-                    self.obstacle_msg.obstacles[i].polygon.points = [Point32()]
-                    self.obstacle_msg.obstacles[i].polygon.points[0].x = -distance_y
-                    self.obstacle_msg.obstacles[i].polygon.points[0].y = 0
-                    self.obstacle_msg.obstacles[i].polygon.points[0].z = distance_x
-                    self.obstacle_msg.obstacles[i].radius = 0.15
-                    self.marker_data.markers.append(Marker())
-                    self.marker_data.markers[i].header.stamp, self.marker_data.markers[i].header.frame_id = rospy.Time.now(), Image.header.frame_id     
-                    self.marker_data.markers[i].ns, self.marker_data.markers[i].id = "basic_shapes", i
-                    #print(tf.transformations.quaternion_from_euler(-math.pi/2, 0, -math.pi/2))
-                    #print(tf.transformations.quaternion_from_euler(math.pi/2, math.pi/2, -math.pi/2))
-                    self.marker_data.markers[i].action = Marker.ADD
-                    self.marker_data.markers[i].pose.position.x, self.marker_data.markers[i].pose.position.y, self.marker_data.markers[i].pose.position.z = -distance_y, 0,distance_x
-                    self.marker_data.markers[i].pose.orientation.x, self.marker_data.markers[i].pose.orientation.y, self.marker_data.markers[i].pose.orientation.z, self.marker_data.markers[i].pose.orientation.w= -0.5, -0.5, -0.5, 0.5
-                    #For Tree
-                    #self.marker_data.markers[i].pose.orientation.x, self.marker_data.markers[i].pose.orientation.y, self.marker_data.markers[i].pose.orientation.z, self.marker_data.markers[i].pose.orientation.w= 0.5, -0.5, 0.5, 0.5
-                    #For Person
-                    #self.marker_data.markers[i].pose.orientation.x, self.marker_data.markers[i].pose.orientation.y, self.marker_data.markers[i].pose.orientation.z, self.marker_data.markers[i].pose.orientation.w= 0.7071, 0, -0.7071, 0
-                    self.marker_data.markers[i].color.r, self.marker_data.markers[i].color.g, self.marker_data.markers[i].color.b, self.marker_data.markers[i].color.a = 1, 0, 0, 1.0
-                    self.marker_data.markers[i].scale.x, self.marker_data.markers[i].scale.y, self.marker_data.markers[i].scale.z = 0.2, 0.2, 1
-                    self.marker_data.markers[i].type = 3
-                    self.marker_data.markers[i].lifetime = rospy.Duration.from_sec(2.0)
-                    #self.marker_data.markers[i].mesh_resource = "package://robot_mower_2dnav/stl/Tree1.stl"                    
-                    #self.marker_data.markers[i].mesh_resource = "package://robot_mower_2dnav/stl/hades.stl"
-                    #self.marker_data.markers[i].mesh_resource = "package://robot_mower_2dnav/stl/animated_walking_man.mesh"
-                    #angle_y = arctan(camera_param[1][0]*center_x+camera_param[1][1]*center_y+camera_param[1][2]*1)
-                    #self.detected_area.flags.writeable = True
-                    #self.detected_area = np.where(self.detected_area < np.percentile(self.detected_area[np.nonzero(self.detected_area)], 40), 0, self.detected_area)
-                    #self.detected_area = np.where(self.detected_area > np.percentile(self.detected_area[np.nonzero(self.detected_area)], 60), 0, self.detected_area)
-                    #self.detected_area = np.where(self.detected_area < np.median(self.detected_area), 0, self.detected_area)
-                    #print("distance_x" + str(distance_x) + "distance_y" + str(distance_y) + "angle_x" + str(math.degrees(angle_x)))
-                    #print("difference" + str(np.median(self.detected_area.nonzero())-np.median(self.detected_area)))
-                    i += 1
+                        self.obstacle_msg.obstacles.append(ObstacleMsg())
+                        self.obstacle_msg.obstacles[i].header.stamp, self.obstacle_msg.obstacles[i].header.frame_id = rospy.Time.now(), Image.header.frame_id     
+                        self.obstacle_msg.obstacles[i].id = i
+                        self.obstacle_msg.obstacles[i].polygon.points = [Point32()]
+                        self.obstacle_msg.obstacles[i].polygon.points[0].x = -distance_y
+                        self.obstacle_msg.obstacles[i].polygon.points[0].y = 0
+                        self.obstacle_msg.obstacles[i].polygon.points[0].z = distance_x
+                        self.obstacle_msg.obstacles[i].radius = 0.15
+                        self.marker_data.markers.append(Marker())
+                        self.marker_data.markers[i].header.stamp, self.marker_data.markers[i].header.frame_id = rospy.Time.now(), Image.header.frame_id     
+                        self.marker_data.markers[i].ns, self.marker_data.markers[i].id = "basic_shapes", i
+                        #print(tf.transformations.quaternion_from_euler(-math.pi/2, 0, -math.pi/2))
+                        #print(tf.transformations.quaternion_from_euler(math.pi/2, math.pi/2, -math.pi/2))
+                        self.marker_data.markers[i].action = Marker.ADD
+                        self.marker_data.markers[i].pose.position.x, self.marker_data.markers[i].pose.position.y, self.marker_data.markers[i].pose.position.z = -distance_y, 0,distance_x
+                        self.marker_data.markers[i].pose.orientation.x, self.marker_data.markers[i].pose.orientation.y, self.marker_data.markers[i].pose.orientation.z, self.marker_data.markers[i].pose.orientation.w= -0.5, -0.5, -0.5, 0.5
+                        #For Tree
+                        #self.marker_data.markers[i].pose.orientation.x, self.marker_data.markers[i].pose.orientation.y, self.marker_data.markers[i].pose.orientation.z, self.marker_data.markers[i].pose.orientation.w= 0.5, -0.5, 0.5, 0.5
+                        #For Person
+                        #self.marker_data.markers[i].pose.orientation.x, self.marker_data.markers[i].pose.orientation.y, self.marker_data.markers[i].pose.orientation.z, self.marker_data.markers[i].pose.orientation.w= 0.7071, 0, -0.7071, 0
+                        self.marker_data.markers[i].color.r, self.marker_data.markers[i].color.g, self.marker_data.markers[i].color.b, self.marker_data.markers[i].color.a = 1, 0, 0, 1.0
+                        self.marker_data.markers[i].scale.x, self.marker_data.markers[i].scale.y, self.marker_data.markers[i].scale.z = 0.2, 0.2, 1
+                        self.marker_data.markers[i].type = 3
+                        self.marker_data.markers[i].lifetime = rospy.Duration.from_sec(2.0)
+                        #self.marker_data.markers[i].mesh_resource = "package://robot_mower_2dnav/stl/Tree1.stl"                    
+                        #self.marker_data.markers[i].mesh_resource = "package://robot_mower_2dnav/stl/hades.stl"
+                        #self.marker_data.markers[i].mesh_resource = "package://robot_mower_2dnav/stl/animated_walking_man.mesh"
+                        #angle_y = arctan(camera_param[1][0]*center_x+camera_param[1][1]*center_y+camera_param[1][2]*1)
+                        #self.detected_area.flags.writeable = True
+                        #self.detected_area = np.where(self.detected_area < np.percentile(self.detected_area[np.nonzero(self.detected_area)], 40), 0, self.detected_area)
+                        #self.detected_area = np.where(self.detected_area > np.percentile(self.detected_area[np.nonzero(self.detected_area)], 60), 0, self.detected_area)
+                        #self.detected_area = np.where(self.detected_area < np.median(self.detected_area), 0, self.detected_area)
+                        #print("distance_x" + str(distance_x) + "distance_y" + str(distance_y) + "angle_x" + str(math.degrees(angle_x)))
+                        #print("difference" + str(np.median(self.detected_area.nonzero())-np.median(self.detected_area)))
+                        i += 1
                 except CvBridgeError as e:
                         pass
 
