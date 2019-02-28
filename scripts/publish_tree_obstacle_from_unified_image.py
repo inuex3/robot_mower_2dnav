8f261ad44e2a1462aaf62695a6af99d636354734
Kosuke Inoue <mmts0106@gmail.com>
2019-02-28 19:12:44 +0900

    Updated working!

--- scripts/publish_tree_obstacle_from_unified_image.py
+++ scripts/publish_tree_obstacle_from_unified_image.py
@@ -8,7 +8,7 @@ from sensor_msgs.msg import Image,CameraInfo
 from cv_bridge import CvBridge, CvBridgeError
 from costmap_converter.msg import ObstacleArrayMsg, ObstacleMsg
 from visualization_msgs.msg import Marker, MarkerArray
-from geometry_msgs.msg import PolygonStamped, Point32
+from geometry_msgs.msg import Point32
 import numpy as np
 from darknet_ros_msgs.msg import BoundingBoxes,BoundingBox
 from rtabmap_ros.srv import ResetPose
@@ -21,15 +21,17 @@ class Publishsers():
     def __init__(self):
         # Publisherを作成
         self.publisher = rospy.Publisher('/move_base/TebLocalPlannerROS/obstacles', ObstacleArrayMsg, queue_size=1)
-        self.marker_publisher = rospy.Publisher("/detected_tree", MarkerArray, queue_size = 1)
+        self.marker_publisher = rospy.Publisher("/visualized_obstacle", MarkerArray, queue_size = 1)
         self.reset_pose = rospy.ServiceProxy('/rtabmap/reset_odom_to_pose', ResetPose)
         self.obstacle_list = ["person", "tree"]
         self.tf_br = tf.TransformBroadcaster()
         self.tf_listener = tf.TransformListener()
+        self.obstacle_msg = ObstacleArrayMsg() 
+        self.marker_data = MarkerArray()
         self.now = rospy.get_rostime()
         self.prev = rospy.get_rostime()
 
-    def make_msg(self, Depth1Image, Depth2Image, detection_data, camera1_param, camera2s_param):
+    def make_msg(self, Depth1Image, Depth2Image, detection_data, camera1_param, camera2_param):
         bboxes_from_camera1 = BoundingBoxes()
         bboxes_from_camera2 = BoundingBoxes()
         bboxes_from_camera1.header = Depth1Image.header
@@ -49,7 +51,7 @@ class Publishsers():
         except CvBridgeError as e:
             print(e)
         for bbox in detection_data.bounding_boxes:
-            if (bbox.ymin + bbox.ymax) < Depth1image.shape[0]:
+            if (bbox.ymin + bbox.ymax)/2 < Depth1image.shape[0]:
                 if bbox.ymax > Depth1image.shape[0]:
                     bbox.ymax = Depth1image.shape[0]
                 bboxes_from_camera1.bounding_boxes.append(bbox)
@@ -59,51 +61,55 @@ class Publishsers():
                 if bbox.ymin < 0:
                     bbox.ymin = 0
                 bboxes_from_camera2.bounding_boxes.append(bbox)
+        camera1_obstacle_msg, camera2_obstacle_msg = ObstacleArrayMsg(), ObstacleArrayMsg()
+        camera1_marker_data, camera2_marker_data = MarkerArray(), MarkerArray()
         camera1_obstacle_msg, camera1_marker_data = self.bbox_to_position_in_odom(bboxes_from_camera1, Depth1image, camera1_param)
-        camera2_obstacle_msg, camera2_marker_data = self.bbox_to_position_in_odom(bboxes_from_camera2, Depth2image, camera2_param)
-        self.obstacle_msg.obstacles.append(camera1_obstacle_msg.obstacles)
-        self.obstacle_msg.obstacles.append(camera2_obstacle_msg.obstacles)
-        self.marker_data.markers.append(camera1_marker_data.markers)
-        self.marker_data.markers.append(camera2_marker_data.markers)
+        camera2_obstacle_msg, camera2_marker_data = self.bbox_to_position_in_odom(bboxes_from_camera2, Depth2image, camera2_param, len(camera1_obstacle_msg.obstacles), camera1_obstacle_msg, camera1_marker_data)
+        self.obstacle_msg.obstacles.extend(camera1_obstacle_msg.obstacles)
+        self.obstacle_msg.obstacles.extend(camera2_obstacle_msg.obstacles)
+        self.marker_data.markers.extend(camera1_marker_data.markers)
+        self.marker_data.markers.extend(camera2_marker_data.markers)
 
     def send_msg(self):
         self.publisher.publish(self.obstacle_msg)
-        self.marker_publisher.publish(self.marker_data)        
+        self.marker_publisher.publish(self.marker_data)      
     
-    def bbox_to_position_in_odom(self, bboxes, DepthImage, camera_param):
-        obstacle_msg = ObstacleArrayMsg() 
-        marker_data = MarkerArray()
-        for i, bbox in enumerate(bboxes):
+    def bbox_to_position_in_odom(self, bboxes, DepthImage, camera_param, i=0, obstacle_msg=ObstacleArrayMsg(), marker_data=MarkerArray()):
+        if i == 0:
+            del obstacle_msg.obstacles[:]
+            del marker_data.markers[:]
+        for bbox in bboxes.bounding_boxes:
             if bbox.Class in self.obstacle_list:
                 try:
-                    tan_angle_x = camera_param[0][0]*(bbox.xmin+bbox.xmax)/2+camera_param[0][1]*(bbox.ymin+bbox.ymax)/2+camera_param[0][2]*1                    
+                    tan_angle_x = camera_param[0][0]*(bbox.xmin+bbox.xmax)/2+camera_param[0][1]*(bbox.ymin+bbox.ymax)/2+camera_param[0][2]*1 
                     angle_x = math.atan(tan_angle_x)
-                    if abs(math.degrees(angle_x)) < 30:
+                    if abs(math.degrees(angle_x)) < 40:
                         detected_area = DepthImage[bbox.ymin:bbox.ymax,  bbox.xmin:bbox.xmax]
                         distance_x = np.median(detected_area)/1000
                         distance_y = - distance_x * tan_angle_x
                         if distance_x < 4:
                             obstacle_msg.obstacles.append(ObstacleMsg())
-                            self.tf_br.sendTransform((-distance_y, 0, distance_x), tf.transformations.quaternion_from_euler(0, 0, 0), bboxes.header.stamp, DepthImage.header.frame_id, bbox.Class + str(i))
-                            obstable_position = self.tf_listener.lookupTransform("odom", bbox.Class + str(i), bboxes.header.stamp)
-                            obstacle_msg.obstacles.append(ObstacleMsg())
-                            obstacle_msg.obstacles[i].header.stamp, self.obstacle_msg.obstacles[i].header.frame_id = bboxes.header.stamp, "odom"    
+                            marker_data.markers.append(Marker())
+                            self.tf_br.sendTransform((-distance_y, 0, distance_x), tf.transformations.quaternion_from_euler(0, 0, 0), rospy.Time.now(), bbox.Class + str(i), bboxes.header.frame_id)
+                            self.tf_listener.waitForTransform("/odom", "/" + bbox.Class + str(i), rospy.Time(0), rospy.Duration(0.1))
+                            obstable_position = self.tf_listener.lookupTransform("odom", bbox.Class + str(i),  rospy.Time(0))
+                            obstacle_msg.obstacles[i].header.stamp, obstacle_msg.obstacles[i].header.frame_id = bboxes.header.stamp, "odom"    
                             obstacle_msg.obstacles[i].id = i
                             obstacle_msg.obstacles[i].polygon.points = [Point32()]
-                            obstacle_msg.obstacles[i].polygon.points[0].x = obstable_position[0]
-                            obstacle_msg.obstacles[i].polygon.points[0].y = obstable_position[1]
-                            obstacle_msg.obstacles[i].polygon.points[0].z = 0
+                            obstacle_msg.obstacles[i].polygon.points[0].x = obstable_position[0][0]
+                            obstacle_msg.obstacles[i].polygon.points[0].y = obstable_position[0][1]
+                            obstacle_msg.obstacles[i].polygon.points[0].z = obstable_position[0][2]
                             obstacle_msg.obstacles[i].radius = 0.15  
-                            marker_data.markers.append(Marker())
                             marker_data.markers[i].header.stamp, marker_data.markers[i].header.frame_id = bboxes.header.stamp, "odom"     
                             marker_data.markers[i].ns, marker_data.markers[i].id = bbox.Class, i
                             marker_data.markers[i].action = Marker.ADD
-                            marker_data.markers[i].pose.position.x, marker_data.markers[i].pose.position.y, marker_data.markers[i].pose.position.z = obstable_position[0], obstable_position[1], 0
-                            marker_data.markers[i].pose.orientation.x, marker_data.markers[i].pose.orientation.y, marker_data.markers[i].pose.orientation.z, marker_data.markers[i].pose.orientation.w= -0.5, -0.5, -0.5, 0.5
-                            marker_data.markers[i].color.r, marker_data.markers[i].color.g, marker_data.markers[i].color.b, marker_data.markers[i].color.a = 1, 0, 0, 1.0
+                            marker_data.markers[i].pose.position.x, marker_data.markers[i].pose.position.y, marker_data.markers[i].pose.position.z = obstable_position[0][0], obstable_position[0][1], obstable_position[0][2]
+                            marker_data.markers[i].pose.orientation.x, marker_data.markers[i].pose.orientation.y, marker_data.markers[i].pose.orientation.z, marker_data.markers[i].pose.orientation.w= tf.transformations.quaternion_from_euler(0, 0, 0)
+                            marker_data.markers[i].color.r, marker_data.markers[i].color.g, marker_data.markers[i].color.b, marker_data.markers[i].color.a = 1, 0, 0, 1
                             marker_data.markers[i].scale.x, marker_data.markers[i].scale.y, marker_data.markers[i].scale.z = 0.2, 0.2, 1
                             marker_data.markers[i].type = 3
                             marker_data.markers[i].lifetime = rospy.Duration.from_sec(3.0)
+                            i = i + 1
                 except Exception as e:
                     print(e)
         return obstacle_msg, marker_data
